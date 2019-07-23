3b898834e4341dc575fb1094cf99cf68309fa29c
magdalsen <47006969+magdalsen@users.noreply.github.com>
2019-07-23 19:17:11 +0200

    Create uniqid.php

---
+++ licznik/uniqid.php
@@ -0,0 +1,17 @@
+<?php
+//Nadaje użytkownikowi ID i liczy wejścia na stronę dla tego ID. W ciastku przechowywana jest liczba wejść na stronę :)
+session_start();
+$dni=365;
+if(!isset($_COOKIE['visits'])) {
+    $ilewizyt = 1;
+    $_SESSION['uniqid'] = uniqid();
+    $uniqid = uniqid();
+    setCookie('visits', "$ilewizyt", time()+60*60*24*$dni);
+    echo "Jesteś na tej stronie pierwszy raz, witamy!";
+} elseif(isset($_COOKIE['visits'])) {
+    $ilewizyt = intval($_COOKIE['visits']);
+    $ilewizyt++;
+    setCookie('visits', "$ilewizyt", time()+60*60*24*$dni);
+    echo "Jesteś na tej stronie $ilewizyt raz, Twoje ID to: ".$_SESSION['uniqid'];
+}
+?>
