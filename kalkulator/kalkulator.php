2df6e8c036243b2e996c990ae8180615fce757e8
magdalsen <47006969+magdalsen@users.noreply.github.com>
2019-07-23 19:13:42 +0200

    Update kalkulator.php

--- kalkulator/kalkulator.php
+++ kalkulator/kalkulator.php
@@ -1,1 +1,20 @@
-
+//Wpisujemy liczby oraz znak
+<!DOCTYPE html>
+<html>
+<head>
+    
+</head>
+<body>
+        <form action="kalkulator2.php" method="POST">    
+        <form>    
+        Kalkulator<br/><br/>
+                Liczba 1:
+                    <input type="number" name="liczba1"><br/><br/>
+                Liczba 2:
+                    <input type="number" name="liczba2"><br/><br/>
+                Znak:
+                    <input type="text" name="znak"><br/><br/>
+                    <input type="submit" name="submit" value="Wynik"><br/><br/>
+        </form>
+</body>
+</html>
